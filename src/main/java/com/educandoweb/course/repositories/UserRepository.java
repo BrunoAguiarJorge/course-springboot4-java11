c23f7c33eb4f28e72832304998496b3a194db2c2
Bruno Aguiar <brunoaguiar@Brunos-MacBook-Air.local>
2019-10-15 16:05:55 +0100

    OrderStatus enum part 2

--- src/main/java/com/educandoweb/course/repositories/UserRepository.java
+++ src/main/java/com/educandoweb/course/repositories/UserRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.educandoweb.course.entities.User;
 
 
-public interface UserRepositoty extends JpaRepository<User, Long>{
+public interface UserRepository extends JpaRepository<User, Long>{
 
 }
